b98b380348cbd59fa5f41ffb64aa70d09dc24627
Jus <julia.rosa.brito@escola.pr.gov.br>
2025-06-24 23:35:22 +0000

    Adicionei uma contagem da frequência das palavras do texto.

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavrasChave = processaTexto(texto);
 
-    campoResultado.textContent = texto;
+    campoResultado.textContent = palavrasChave.join(", ");
+}
+
+
+function processaTexto(texto){
+    let palavras = texto.split(/\P{L}+/u);
+
+    let frequencias = [];
+    for(let i of palavras){
+        frequencias[i] = 0;
+        for (let j of palavras){
+            if (palavras[i]==palavras[j]){
+                frequencias[i]++;
+            }
+        }
+    }
+
+    console.log(frequencias);
+
+    return palavras;
 }
