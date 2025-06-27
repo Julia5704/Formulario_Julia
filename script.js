e106aa4666594ea84791042fcb1f22a9296eb189
Jus <julia.rosa.brito@escola.pr.gov.br>
2025-06-27 03:21:31 +0000

    Criei uma função para "tirar palavras ruins".

--- script.js
+++ script.js
@@ -6,15 +6,28 @@ function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
     const palavrasChave = processaTexto(texto);
-
     campoResultado.textContent = palavrasChave.join(", ");
 }
 
-
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
+    for(let i in palavras){
+        palavras[i] = palavras[i] .toLowerCase();
+    }
 
-    let frequencias = [];
+    palavras = tiraPalavrasRuins(palavras);
+
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys (frequencias).sort(ordenaPalavra);
+    function ordenaPalavra (p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+    console.log(ordenadas);
+    return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras){
+    let frequencias = {};
     for(let i of palavras){
         frequencias[i] = 0;
         for (let j of palavras){
@@ -23,8 +36,16 @@ function processaTexto(texto){
             }
         }
     }
+    return frequencias;
+}
 
-    console.log(frequencias);
-
-    return palavras;
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new Set (["para", "uma", "nós"]);
+    const palavrasBoas = [];
+    for (let palavra of palavras){
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+    palavrasBoas.push(palavra);
+        }
+    }
+return palavrasBoas;
 }
